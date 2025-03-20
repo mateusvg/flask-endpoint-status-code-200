619d3be452921bc07e52a1a90211a10746ff5095
Mateus Vitorino Guimaraes <mateusvitorino@gmail.com>
2025-03-20 08:59:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import logging
 from flask import Flask, jsonify
+
+# Configuração do logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
 @app.route('/notificacao', methods=['GET'])
 def notificacao():
+    logger.info("Requisição recebida em /notificacao")
     return jsonify({"mensagem": "Notificação recebida"}), 200
 
 if __name__ == '__main__':
